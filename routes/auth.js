860aed7e61554f7cdf4e308405594f54dbdcf71b
Afsal P U <afsalpu16@gmail.com>
2023-02-28 10:01:13 +0530

    login fixed

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { User, } = require("../models/user");
-// const bcrypt = require("bcrypt");
 const CryptJS = require('crypto-js')
+const jwt  = require('jsonwebtoken')
 
 router.post("/", async (req, res) => {
     const user = await User.findOne({email: req.body.email})
@@ -11,8 +11,16 @@ router.post("/", async (req, res) => {
     const validPassword = await CryptJS.AES.decrypt(req.body.password, process.env.SECRET_KEY)
     if(!validPassword)
         return res.status(400).send({message: "Invalid username or password!"})
+    
+    const token = jwt.sign({
+        id: user._id,
+        isAdmin: user.isAdmin,
+        name: user.name
+    }, process.env.JWTPRIVATEKEY, {expiresIn: "7d"})
 
-    res.status(200).send({ message: "Signin please wait..." }) 
+    const {password, ...others} = user._doc;
+
+    res.status(200).send({ message: "Signin please wait...", data: {...others, token } }) 
  
     })
 
